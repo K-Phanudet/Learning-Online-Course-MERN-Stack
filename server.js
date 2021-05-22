0e67b2aba62b0d90963eb25954c0167a04b3fe5e
K-Phanudet <k.phanudet.work@gmail.com>
2021-05-22 16:56:59 +0700

    add routes controller

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000
+const {mongoConnect} = require('./config/db')
 
+const app = express();
+const api = express()
+const PORT = process.env.PORT || 5000
+app.use(express.json({extended:true}))
+// Connect db
+mongoConnect()
+
+// Init middleware
 app.get('/',(req,res)=>res.send('API running'))
+
+// Defines Routes
+api.use('/users',require('./routes/api/users'))
+api.use('/profile',require('./routes/api/profile'))
+api.use('/posts',require('./routes/api/posts'))
+api.use('/auth',require('./routes/api/auth'))
+
+app.use('/api',api)
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server started on port ${PORT}`)
